d2e7c04027a0482964951a37f20f35b08111fabf
Simon Gratton <sigratton@gmail.com>
2017-08-04 12:45:41 +0800

    Get all list working

--- controllers/ListController.ts
+++ controllers/ListController.ts
@@ -5,12 +5,27 @@ import { WordList, IWordList } from '../models/WordList';
 export default class ListController{
     constructor(server: Server, db: Connection) {
         server.get('/list/:id', this.GetList);
+        server.get('/list', this.GetList);
         server.post('/list', this.PostList);
     }
 
     public GetList(req: Request, res: Response, next: Next) {
-        res.send('hello ' + req.params.id);
-        next();
+        if(req.params.id) {
+            res.send('hello ' + req.params.id);
+            next();
+        }
+        else {
+            WordList.find({}, (err: any, wordlist: IWordList[]) => {
+                if(err) {
+                    res.send(500, err);
+                    next();
+                    return;
+                }
+                
+                res.send(wordlist);
+                next();
+            });
+        }
     }
 
     public PostList(req: Request, res: Response, next: Next) {
